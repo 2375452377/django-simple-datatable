9106f63dc6752f8b32558dd067d31686b61f5d39
terrence <terrence.luo@coassets.com.cn>
2019-11-08 15:03:11 +0800

    添加 tox.ini

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,1 @@
 #!/usr/bin/python
-from django.test.runner import DiscoverRunner
-
-
-class DJETTestSuiteRunner(DiscoverRunner):
-    def setup_test_environment(self, **kwargs):
-        super(DJETTestSuiteRunner, self).setup_test_environment(**kwargs)
